2d4e4ed9c5d44abb4084f09fed6cb6b5edd26564
Estevao Adam <estevao.rafael@cactusgaming.net>
2024-02-29 17:20:22 -0300

    tweaking css to look like figma for mobile

--- src/components/VotesComponent.vue
+++ src/components/VotesComponent.vue
@@ -49,17 +49,18 @@ const getPercentage = (a: number, b: number, actualVote: number) => {
         <img :src="`${vote.picture}`" alt="thumbs up" />
       </div>
       -->
-      <div class="featured-card__buttons">
-        <button class="icon-button" aria-label="thumbs up">
+      <div class="middle-buttons">
+        <div class="middle-items">
           <img src="@/assets/img/thumbs-up.svg" alt="thumbs up" />
-        </button>
-        <button class="icon-button" aria-label="thumbs down">
+        </div>
+        <div class="middle-items">
           <img src="@/assets/img/thumbs-down.svg" alt="thumbs down" />
-        </button>
-        <button class="vote-button">Vote Now</button>
+        </div>
+        <div class="middle-items">
+          <button class="vote-button">Vote Now</button>
+        </div>
       </div>
       <div class="voting-stats">
-        <img src="@/assets/img/thumbs-up.svg" alt="thumbs up" />
         <div
           class="voting-positive"
           :style="{
@@ -71,13 +72,20 @@ const getPercentage = (a: number, b: number, actualVote: number) => {
               ) + '%'
           }"
         >
-          {{
-            getPercentage(
-              vote.votes.negative,
-              vote.votes.positive,
-              vote.votes.positive
-            ) + '%'
-          }}
+          <div class="flex-votes">
+            <div class="flex-positive">
+              <img src="@/assets/img/thumbs-up.svg" alt="thumbs up" />
+            </div>
+            <div class="flex-positive">
+              {{
+                getPercentage(
+                  vote.votes.negative,
+                  vote.votes.positive,
+                  vote.votes.positive
+                ) + '%'
+              }}
+            </div>
+          </div>
         </div>
 
         <div
@@ -91,15 +99,22 @@ const getPercentage = (a: number, b: number, actualVote: number) => {
               ) + '%'
           }"
         >
-          {{
-            getPercentage(
-              vote.votes.negative,
-              vote.votes.positive,
-              vote.votes.negative
-            ) + '%'
-          }}
+          <div class="flex-votes">
+            <div class="flex-negative">
+              {{
+                getPercentage(
+                  vote.votes.negative,
+                  vote.votes.positive,
+                  vote.votes.negative
+                ) + '%'
+              }}
+            </div>
+
+            <div class="flex-negative">
+              <img src="@/assets/img/thumbs-down.svg" alt="thumbs up" />
+            </div>
+          </div>
         </div>
-        <img src="@/assets/img/thumbs-down.svg" alt="thumbs up" />
       </div>
     </div>
   </div>
@@ -111,6 +126,33 @@ const getPercentage = (a: number, b: number, actualVote: number) => {
 
   margin: 1em 0;
 }
+.middle-buttons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 90%;
+  font-size: 1rem;
+  height: 20%;
+  margin-bottom: 1em;
+}
+.middle-items {
+  height: 100%;
+  padding-left: 0.5em;
+  width: auto;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  padding-left: 1.5em;
+}
+.middle-items img[alt='thumbs up'] {
+  padding: 1em;
+
+  background-color: rgba(var(--color-green-positive), 1);
+}
+.middle-items img[alt='thumbs down'] {
+  padding: 1em;
+  background-color: rgba(var(--color-yellow-negative), 0.8);
+}
 .picture-img {
   width: 100%;
   height: 200px;
@@ -118,11 +160,25 @@ const getPercentage = (a: number, b: number, actualVote: number) => {
   background-size: cover;
   background-position: center;
 }
+.flex-votes {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  font-size: 1.5rem;
+  color: white;
+}
+.flex-positive {
+  padding-left: 0.8em;
+}
+.flex-negative {
+  padding-right: 0.8em;
+}
 .vote-button {
   background-color: #525252b6;
   color: white;
   border: 5px solid white;
-  width: 30%;
+  width: 100%;
   font-size: 1rem;
 }
 .voting-stats {
@@ -132,10 +188,10 @@ const getPercentage = (a: number, b: number, actualVote: number) => {
 }
 .voting-positive {
   height: 3rem;
-  background-color: rgba(0, 128, 128, 0.758);
+  background-color: rgba(var(--color-green-positive), 1);
 }
 .voting-negative {
   height: 3rem;
-  background-color: #c1923bbb;
+  background-color: rgba(var(--color-yellow-negative), 0.8);
 }
 </style>
