de932fa22dee4a6a25cec67d92f856b993cf8dba
Estevao Adam <estevao.rafael@cactusgaming.net>
2024-02-29 13:45:08 -0300

    needs to find the percentage of the votes and make the width of the div accordingly

--- src/components/VotesComponent.vue
+++ src/components/VotesComponent.vue
@@ -1,10 +1,34 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 import { useVotingStore } from '@/stores/voting'
 import { storeToRefs } from 'pinia'
+//import type { Votes } from '@/types/VotesData'
 
+interface Votes {
+  name: string
+  description: string
+  category: string
+  picture: string
+  lastUpdated: string
+  votes: {
+    positive: number
+    negative: number
+  }
+}
 const { votingData } = storeToRefs(useVotingStore())
+
+const fullVotes = (a: number, b: number) => {
+  return a + b
+}
+
+const getPercentage = (a: number, b: number, actualVote: number) => {
+  const allVotes = fullVotes(a, b)
+
+  console.log((actualVote / allVotes) * 100)
+  const result = (actualVote / allVotes) * 100
+  return result.toFixed(1)
+}
 </script>
 <template>
   <div v-for="vote in votingData" :key="vote.name">
@@ -24,15 +48,28 @@ const { votingData } = storeToRefs(useVotingStore())
       </button>
       <button class="vote-button">Vote Now</button>
     </div>
-
-    <div class="hero__closing-gauge">
-      <div class="closing-gauge__left">
-        <span class="closing-gauge__title">closing in</span>
+    <div class="voting-stats">
+      <div
+        class="voting-positive"
+        :style="{
+          width:
+            getPercentage(
+              vote.votes.negative,
+              vote.votes.positive,
+              vote.votes.positive
+            ) + '%'
+        }"
+      >
+        {{
+          getPercentage(
+            vote.votes.negative,
+            vote.votes.positive,
+            vote.votes.positive
+          ) + '%'
+        }}
       </div>
-      <div class="closing-gauge__right">
-        <span class="closing-gauge__number">22</span>
-        <span class="closing-gauge__desc">days</span>
-      </div>
+      <div class="voting-negative"></div>
+      <span class="closing-gauge__desc">e</span>
     </div>
   </div>
 </template>
@@ -44,5 +81,14 @@ const { votingData } = storeToRefs(useVotingStore())
   border: 5px solid white;
   width: 30%;
   font-size: 1rem;
+}
+.voting-stats {
+  display: flex;
+  width: 100%;
+  height: 3rem;
+}
+.voting-positive {
+  height: 3rem;
+  background-color: rgba(0, 128, 128, 0.758);
 }
 </style>
--- src/stores/voting.ts
+++ src/stores/voting.ts
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 import votingData from '@/data/data.json'
-//import { Votes } from '@/types/VotesData'
+//import type { Votes } from '@/types/VotesData'
 
 export const useVotingStore = defineStore('voting', {
   state: () => ({
